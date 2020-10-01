1ec4709eb552b2bfc2e10a1e2e263421be64f401
BklynIrish <bm624@nyu.edu>
2020-10-01 13:01:41 -0400

    made some changes to the code and aesthetic changes

--- app.js
+++ app.js
@@ -1,57 +1,50 @@
 var sampleData = "data/samples.json"
 
-//get subjectIDs so we can populate the pull down list.
+//get bacteriaIDs so we can populate the pull down list.
 var data = {};
 d3.json(sampleData).then(function(data) {
-        console.log(this);
-        var subjectIDs = data.names;
-        console.log(subjectIDs);
-        updateIDList(subjectIDs);
+        var bacteriaIDs = data.names;
+
+        updateIDList(bacteriaIDs);
         var selectedID = d3.select("#selDataset");
         //selectedID.on("change", runFilter);
         document.getElementById("selDataset").addEventListener("change", runFilter);
-        var theRow = {}
+        var bactId = {}
 
         function runFilter() {
-            //the code on line 16 cqptures users demographic input
-            var idValue = String(d3.select("#selDataset").node().value);
-            console.log("runfilter", d3.select("#selDataset"));
-            console.log("here", idValue);
-            //line 20 is filtering the data corresponding to the id that the user selects
-            data.metadata.forEach(function(row) {
-                    if (row.id == idValue) {
-                        metaData = row
-                        console.log(row)
-                    }
-                })
-                // this can also be accomplished by filtering as we did in line 22
-            data.samples.forEach(function(name) {
-                if (name.id == idValue) {
-                    theRow = name
-                    console.log(name)
-                }
-            })
-            console.log(theRow)
 
-            console.log("print keys ", Object.entries(metaData));
+            var idValue = d3.select("#selDataset").node().value;
+
+            //line 20 is filtering the data corresponding to the id that the user selects
+            var metaData = data.metadata.filter(
+                (sampleObj) => sampleObj.id == idValue
+            )[0];
+
+            var bactId = data.samples.filter(
+                (sampleObj) => sampleObj.id == idValue
+            )[0];
+
+
+
             populateDemo(metaData);
 
-            var topSampleValues = theRow.sample_values.slice(0, 10).reverse();
-            var topOTUIDs = theRow.otu_ids.slice(0, 10).reverse();
+            var topSampleValues = bactId.sample_values.slice(0, 10).reverse();
+            var topOTUIDs = bactId.otu_ids.slice(0, 10).reverse();
             var otuString = "OTU ";
             var topStringOTUIDS = topOTUIDs.map(function(item) {
                 return otuString.concat(item.toString());
             })
-            var topOTULabels = theRow.otu_labels.slice(0, 10).reverse();
-            console.log(topSampleValues);
-            console.log(topStringOTUIDS);
-            console.log(topOTULabels);
+            var topOTULabels = bactId.otu_labels.slice(0, 10).reverse();
+
             var trace1 = {
                 y: topStringOTUIDS,
                 x: topSampleValues,
                 type: "bar",
                 orientation: "h",
-                text: topOTULabels
+                text: topOTULabels,
+                marker: {
+                    color: 'magenta'
+                }
             }
             var layout1 = {
                 title: "",
@@ -60,8 +53,8 @@ d3.json(sampleData).then(function(data) {
             var graphData = [trace1];
             Plotly.newPlot("bar", graphData, layout1)
 
-            allColors = randomColor(theRow.otu_ids.length)
-            console.log(allColors);
+            allColors = randomColor(bactId.otu_ids.length)
+
 
             function randomColor(countVar) {
                 var allC = [];
@@ -72,21 +65,21 @@ d3.json(sampleData).then(function(data) {
                     color3 = (Math.floor(Math.random() * 256).toString());
                     c = `rgb(${color1}, ${color2}, ${color3})`
                     allC.push(c);
-                    console.log("allC ", allC);
+
                 }
                 return allC;
             }
-            console.log("all colors ", allColors);
+
             var trace2 = {
-                y: theRow.sample_values,
-                x: theRow.otu_ids,
-                text: theRow.otu_labels,
+                y: bactId.sample_values,
+                x: bactId.otu_ids,
+                text: bactId.otu_labels,
                 mode: 'markers',
                 marker: {
                     color: allColors,
-                    size: theRow.sample_values,
-                    sizeref: 2.0 * Math.max(theRow.sample_values) / 50 ** 2
-                        //color: theRow.otu_ids
+                    size: bactId.sample_values,
+                    sizeref: 2.0 * Math.max(bactId.sample_values) / 50 ** 2
+
                 }
             }
             var layout = {
@@ -118,16 +111,16 @@ d3.json(sampleData).then(function(data) {
                         'tickfont': { 'size': 20 }
                     },
                     'steps': [
-                        { 'range': [0, 1], 'color': 'rgb(255,230,255)' },
-                        { 'range': [1, 2], 'color': 'rgb(255,200,255)' },
-                        { 'range': [2, 3], 'color': 'rgb(255,170,255)' },
-                        { 'range': [3, 4], 'color': 'rgb(255,140,255)' },
-                        { 'range': [4, 5], 'color': 'rgb(255,100,255)' },
-                        { 'range': [5, 6], 'color': 'rgb(255,50,255)' },
-                        { 'range': [6, 7], 'color': 'rgb(255,0,255)' },
-                        { 'range': [7, 8], 'color': 'rgb(200,0,200)' },
-                        { 'range': [8, 9], 'color': 'rgb(150,0,150)' },
-                        { 'range': [9, 10], 'color': 'rgb(100,0,100)' }
+                        { 'range': [0, 1], 'color': 'rgb(0,0,If )' },
+                        { 'range': [1, 2], 'color': 'rgb(25,25,255)' },
+                        { 'range': [2, 3], 'color': 'rgb(50,50,255)' },
+                        { 'range': [3, 4], 'color': 'rgb(75,75,255)' },
+                        { 'range': [4, 5], 'color': 'rgb(100,100,255)' },
+                        { 'range': [5, 6], 'color': 'rgb(125,125,255)' },
+                        { 'range': [6, 7], 'color': 'rgb(150,150,255)' },
+                        { 'range': [7, 8], 'color': 'rgb(175,175,255)' },
+                        { 'range': [8, 9], 'color': 'rgb(200,200,255)' },
+                        { 'range': [9, 10], 'color': 'rgb(225,225,255)' }
 
                     ],
 
@@ -142,25 +135,23 @@ d3.json(sampleData).then(function(data) {
 
 
     })
-    //this is analagous to appinitial lines 24-32
+    //this is analagous to appinitial.js lines 24-32
 function populateDemo(values) {
-    //d3.event.preventDefault();
-    console.log(d3.select("#selDataset"));
+
+
     d3.select("#sample-metadata").selectAll("p").remove();
     Object.entries(values).forEach(([key, value]) => {
-        //var row doesn't have to be in each iteration, the alternative would be to remofe at 139 and replace at 134
+        //var row doesn't have to be in each iteration, the alternative would be to remove at 139 and replace at 134
         var row = d3.select("#sample-metadata");
         row.append("p").text(`${key}: ${value}`);
     })
 }
 
 
-function updateIDList(subjectIDs) {
-    //d3.event.preventDefault();
-    console.log(d3.select("#selDataset"));
+function updateIDList(bacteriaIDs) {
     d3.select("#selDataset").selectAll("option").remove();
     d3.select('#ID').append("option").text("empty");
-    subjectIDs.forEach(function(id) {
+    bacteriaIDs.forEach(function(id) {
         var row = d3.select("#selDataset");
         row.append("option").text(id);
     })
